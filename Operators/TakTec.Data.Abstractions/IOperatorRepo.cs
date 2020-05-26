79181d6c7217ec683b273f9c54e886af8a3dc1a2
Ath3na <tersit1989@gmail.com>
2020-05-26 23:31:16 +0300

    Operator repo added

---
+++ Operators/TakTec.Data.Abstractions/IOperatorRepo.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using TakTec.Data.Entities;
+//using EthioArt.Data.Abstractions;
+namespace TakTec.Data.Abstractions
+{
+    public interface IOperatorRepo//:IGenericRepository<Operator>
+    {
+        
+    } 
+   
+   
+}

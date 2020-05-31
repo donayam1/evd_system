76daf9edc2a428fef6d79ea8544cd36815b8a3e9
Natty-star <59245363+Natty-star@users.noreply.github.com>
2020-05-31 14:06:20 +0300

    Add voucher list

--- HostApp/ClientApp/src/app/data/Voucher/Models/voucherUpload.services.ts
+++ HostApp/ClientApp/src/app/data/Voucher/Models/voucherUpload.services.ts
@@ -4,6 +4,18 @@ export class UploadVoucherResponse extends ResponseBase {
     constructor(obj?: any) {
         super(obj);
     }
+}
 
+export class ListVoucherResponse extends ResponseBase {
+    constructor(obj? : any) {
+        super(obj);
+    }
 
+    id: number;
+    serialNumber: number;
+    pinNumber : number;
+    stopDate : string;
+    denomination : number;
+    voucherStatus: number;         
+ 
 }

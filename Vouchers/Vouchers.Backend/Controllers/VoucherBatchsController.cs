3a3cbda4e7626bb84840d6213da9188068190403
Donayam <donayam.nega@yahoo.com>
2020-06-01 19:23:39 +0300

    Fixed voucher authorization problem error.

---
+++ Vouchers/Vouchers.Backend/Controllers/VoucherBatchsController.cs
@@ -0,0 +1,21 @@
+﻿using Messages.BusinessLogic.Abstraction;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Vouchers.Backend.Controllers
+{
+    public class VoucherBatchsController: VoucherControllersBase
+    {
+        public VoucherBatchsController(IUserMessageLogges logs) :
+            base(logs)
+        {
+            
+        }
+
+
+
+
+
+    }
+}

cbebcdd2cd3e154d9895373a4279f04cb60bdfc9
Natty-star <59245363+Natty-star@users.noreply.github.com>
2020-06-01 13:56:31 +0300

    Add purchase order list

--- HostApp/ClientApp/src/app/purchase-order/purchase-order-routing.module.ts
+++ HostApp/ClientApp/src/app/purchase-order/purchase-order-routing.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { PurchaseOrderHomeComponent } from './purchase-order-home/purchase-order-home.component';
 import { CreatePurchaseOrderComponent } from './create-purchase-order/create-purchase-order.component';
+import { ListPurchaseOrderComponent } from './list-purchase-order/list-purchase-order.component';
 
 const routes: Routes = [
   {path: '', component: PurchaseOrderHomeComponent, children:[
     {path: 'create', component: CreatePurchaseOrderComponent},
-    //{path: 'list', component: ListPurchaseOrderComponent}
+    {path: 'list', component: ListPurchaseOrderComponent}
   ]}
 ];
 

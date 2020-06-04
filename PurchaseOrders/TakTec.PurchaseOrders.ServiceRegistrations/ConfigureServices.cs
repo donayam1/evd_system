514a054b087c8dac9c725ce98c12f18175e11889
Donayam <donayam.nega@yahoo.com>
2020-06-04 09:18:30 +0300

    Commened assemblies.

--- PurchaseOrders/TakTec.PurchaseOrders.ServiceRegistrations/ConfigureServices.cs
+++ PurchaseOrders/TakTec.PurchaseOrders.ServiceRegistrations/ConfigureServices.cs
@@ -1,18 +1,18 @@
-﻿using ExtCore.Infrastructure.Actions;
-using Microsoft.Extensions.DependencyInjection;
-using System;
-using TakTec.PurchaseOrders.BusinessLogic;
-using TakTec.PurchaseOrders.BusinessLogic.Abstractions;
+﻿//using ExtCore.Infrastructure.Actions;
+//using Microsoft.Extensions.DependencyInjection;
+//using System;
+//using TakTec.PurchaseOrders.BusinessLogic;
+//using TakTec.PurchaseOrders.BusinessLogic.Abstractions;
 
 namespace TakTec.PurchaseOrders.ServiceRegistrations
 {
-    public class ConfigureServices : IConfigureServicesAction
-    {
-        public int Priority => 2000;
+    //public class ConfigureServices : IConfigureServicesAction
+    //{
+    //    public int Priority => 2000;
 
-        public void Execute(IServiceCollection services, IServiceProvider serviceProvider)
-        {
-            services.AddScoped<IPurchaseOrderService, PurchaseOrderService>();
-        }
-    }
+    //    public void Execute(IServiceCollection services, IServiceProvider serviceProvider)
+    //    {
+    //        services.AddScoped<IPurchaseOrderService, PurchaseOrderService>();
+    //    }
+    //}
 }

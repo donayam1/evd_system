1e95d2dc54171e77fd749f97c0fe164cdd2aa36b
Natty-star <59245363+Natty-star@users.noreply.github.com>
2020-06-05 16:23:16 +0300

    create user list and detail

--- HostApp/ClientApp/src/app/user/user-routing.module.ts
+++ HostApp/ClientApp/src/app/user/user-routing.module.ts
@@ -2,10 +2,14 @@ import { CreateUserComponent } from './create-user/create-user.component';
 import { UserHomeComponent } from './user-home/user-home.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ListUserComponent } from './list-user/list-user.component';
+import { UserDetailComponent } from './user-detail/user-detail.component';
 
 const routes: Routes = [
   { path: '', component: UserHomeComponent , children:[
-    { path: 'create', component: CreateUserComponent }
+    { path: 'create', component: CreateUserComponent },
+    {path: 'list', component: ListUserComponent},
+    {path: 'detail', component: UserDetailComponent }
   ]}
 ];
 

b0454453ab8223747702e11228c7149dad315fad
Natty-star <59245363+Natty-star@users.noreply.github.com>
2020-06-06 17:09:56 +0300

    voucher batch list

---
+++ HostApp/ClientApp/src/app/voucher-batch/voucher-batch-routing.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ListvoucherBatchComponent } from './listvoucher-batch/listvoucher-batch.component';
+
+const routes: Routes = [
+  {
+    path: '' , component: HomeComponent, children: [
+      {path:'list' , component: ListvoucherBatchComponent }
+    ]
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class VoucherBatchRoutingModule { }

7dac958fa4adbf3c1b4857749859f2bdd6a75d8f
Natnael Tilaye <natnaeltilaye30@gmail.com>
2020-06-06 11:42:37 +0300

    Fixed bug on user service

--- HostApp/ClientApp/src/app/data/User/Services/user.service.ts
+++ HostApp/ClientApp/src/app/data/User/Services/user.service.ts
@@ -10,7 +10,7 @@ import { ObjectStatus } from '../../Shared/Models/newObjectStatus.model';
   providedIn: 'root'
 })
 export class UserService {
-  private readonly api = "";
+  private readonly api = "/api/evd/users/user";
 
   constructor(private http: HttpClient) { }
 
@@ -48,42 +48,42 @@ export class UserService {
 
   createUser(user: NewUser): Observable<NewUserResponse> {
     //Mock Data
-    let response = new NewUserResponse();
-    response.status = true;
+    // let response = new NewUserResponse();
+    // response.status = true;
 
-    let mes = new Message();
-        mes.messageCode = '30';
-        mes.messageType = 1;
-        mes.systemMessage = "user data correctly traverses between service.";
-        response.messages.push(mes);
+    // let mes = new Message();
+    //     mes.messageCode = '30';
+    //     mes.messageType = 1;
+    //     mes.systemMessage = "user data correctly traverses between service.";
+    //     response.messages.push(mes);
 
-    let nu = new NewUser();
-        nu.Id = '1';
-        nu.UserName = user.UserName;
-        nu.PicUrl = "";
-        nu.email = user.email;
-        nu.phoneNumber = user.phoneNumber;
-        nu.firstName = user.firstName;
-        nu.lastName = user.lastName;
-        nu.rankId = user.rankId;
-        nu.planId = '2';
-        nu.objectStatus = user.objectStatus;
+    // let nu = new NewUser();
+    //     nu.Id = '1';
+    //     nu.UserName = user.UserName;
+    //     nu.PicUrl = "";
+    //     nu.email = user.email;
+    //     nu.phoneNumber = user.phoneNumber;
+    //     nu.firstName = user.firstName;
+    //     nu.lastName = user.lastName;
+    //     nu.rankId = user.rankId;
+    //     nu.planId = '2';
+    //     nu.objectStatus = user.objectStatus;
 
-        response.newUser = nu;
+    //     response.newUser = nu;
 
-    return of(response);
+    // return of(response);
 
     //Later to be used with the api.
-    // const url = AppConfig.settings.apiServers.authServer + this.api + "/Create";
-    // return new Observable(observer => {
-    //   this.http.post<NewUserResponse>(url, user).subscribe(x => {
-    //     observer.next(new NewUserResponse(x));
-    //     observer.complete();
-    //   }, error => {
-    //     observer.error(error);
-    //     observer.complete();
-    //   });
-    // });
+    const url = AppConfig.settings.apiServers.authServer + this.api + "/Create";
+    return new Observable(observer => {
+      this.http.post<NewUserResponse>(url, user).subscribe(x => {
+        observer.next(new NewUserResponse(x));
+        observer.complete();
+      }, error => {
+        observer.error(error);
+        observer.complete();
+      });
+    });
   }
 
   getUser(id: string){

bc3481e5a70b8c915b38c170430ff462fa852292
Donayam <donayam.nega@yahoo.com>
2020-06-09 11:00:02 +0300

    Updated project dependencies.

--- Accounting/TakTec.Accounting.BusinessLogic/MoneyDepositService.cs
+++ Accounting/TakTec.Accounting.BusinessLogic/MoneyDepositService.cs
@@ -1,13 +1,22 @@
-﻿using System;
+﻿using ExtCore.Data.Abstractions;
+using System;
+using TakTec.Accounting.Data.Abstractions;
 using TakTec.Accounting.ViewModels;
 
 namespace TakTec.Accounting.BusinessLogic
 {
     public class MoneyDepositService
     {
-        public MoneyDepositService() { 
+        private readonly IMoneyDepositRepository moneyDepositRepository;
+        private readonly IStorage _storage;
+        public MoneyDepositService(IStorage storage) {
+            _storage = storage ?? throw new ArgumentNullException(nameof(IStorage));
         }
 
+
+        bool validateApproveReqeust() {
+            return false;
+        }
 
         public bool ApproveMoneyDeposit(ApproveMoneyDepositRequest request) {
 

50344da0b9567fbcaf3c54916fea367481793376
Natnael Tilaye <natnaeltilaye30@gmail.com>
2020-06-09 11:01:46 +0300

    Added model for UserBankAccount

---
+++ HostApp/ClientApp/src/app/data/UserBankAccount/Models/user-bank-account.model.spec.ts
@@ -0,0 +1,7 @@
+import { UserBankAccount } from './user-bank-account.model';
+
+describe('UserBankAccount', () => {
+  it('should create an instance', () => {
+    expect(new UserBankAccount()).toBeTruthy();
+  });
+});

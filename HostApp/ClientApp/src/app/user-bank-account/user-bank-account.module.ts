a5b6caeef188218132732b94951ab92daac2b768
Natnael Tilaye <natnaeltilaye30@gmail.com>
2020-06-10 13:16:28 +0300

    Added UserBankAccount module

---
+++ HostApp/ClientApp/src/app/user-bank-account/user-bank-account.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { UserBankAccountRoutingModule } from './user-bank-account-routing.module';
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    UserBankAccountRoutingModule
+  ]
+})
+export class UserBankAccountModule { }
